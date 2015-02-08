2bb192ff5efc6c7a7a0bfdd0a8e214298cc365f4
flib99 <josh@wallsfamily.co.uk>
2015-02-08 17:52:38 +0000

    Added loading screen

---
+++ src/app.js
@@ -0,0 +1,16 @@
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
+
+var UI = require('ui');
+
+
+var splash = new UI.Card({
+  title: 'PebbleBeard',
+  subtitle: 'SickBeard for your Pebble',
+  body: 'Loading'
+});
+
+splash.show();
